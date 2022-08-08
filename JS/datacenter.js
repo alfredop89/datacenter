483b17f7e1f77e9053d4e5aa4cecbb937ddcf931
alfredop89 <offsound2020@gmail.com>
2022-08-07 21:51:26 -0400

    actualizando

--- JS/datacenter.js
+++ JS/datacenter.js
@@ -1,14 +1,19 @@
 const dcmodal = document.querySelector("#dc-modal-container")
 const comprar = document.querySelector("#cardbutton")
+    
+
 
 
 const cerrarModal = document.querySelector("#close-modal")
 cerrarModal.addEventListener("click", () =>{
     dcmodal.classList.remove("open-dcmodal")
-    window.classList.remove("open-dcmodal")
+    window.addEventListener("click", () => {
+        dcmodal.onremove("open-modal")
+    })
 })
 
 
 comprar.addEventListener("click", () =>{
     dcmodal.classList.add("open-dcmodal")
 })
+
