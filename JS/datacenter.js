4d42d56f0681e4ad317ea3e1780b6a0aeba272e5
alfredop89 <offsound2020@gmail.com>
2022-08-08 20:16:15 -0400

    actualizando

--- JS/datacenter.js
+++ JS/datacenter.js
@@ -1,31 +1,34 @@
 // MODAL
 const dcmodal = document.querySelector("#dc-modal-container")
-const compra1 = document.querySelector(".btnbasico")
-const compra2 = document.querySelector(".btnnegocios")
-const compra3 = document.querySelector(".btnempresas")
-const compra4 = document.querySelector(".btnpro")
-const closeModal = document.querySelector("#close-modal")
 
+// dcmodal,addEventListener("submit", (Event) =>{
+//     Event.preventDefault()
+// })
+dcmodal.addEventListener("click", () =>{
+    dcmodal.classList.remove(".open-dcmodal")
+})
+const compra1 = document.querySelector(".btnbasico")
 compra1.onclick = () => {
     dcmodal.classList.add("open-dcmodal")
 }
-
+const compra2 = document.querySelector(".btnnegocios")
 compra2.onclick = () => {
     dcmodal.classList.add("open-dcmodal")
 }
-
+const compra3 = document.querySelector(".btnempresas")
 compra3.onclick = () => {
     dcmodal.classList.add("open-dcmodal")
 }
-
+const compra4 = document.querySelector(".btnpro")
 compra4.onclick = () => {
     dcmodal.classList.add("open-dcmodal")
 }
-
+const closeModal = document.querySelector("#close-modal")
 closeModal.onclick = () =>{
     dcmodal.classList.remove("open-dcmodal")
 }
 
-window.addEventListener("click", () => {
-    dcmodal.classList.remove(".open-dcmodal")
-})
+
+
+
+
