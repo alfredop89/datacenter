66785648a13cec92cd7a139d5721e88cca6ddd69
alfredop89 <offsound2020@gmail.com>
2022-09-01 13:41:11 -0400

    añadiendo barra de progreso

--- JS/header.js
+++ JS/header.js
@@ -1,6 +1,26 @@
+// BARRA DE PROGRESO
+
+const barraProgreso = document.querySelector('.barra-progreso')
+
+function loadScroll () {
+
+    let scrollTop = document.documentElement.scrollTop
+    let scrollHeight = document.documentElement.scrollHeight
+    let clientHeight = document.documentElement.clientHeight
+
+    let windowHeight = scrollHeight - clientHeight
+    let load = scrollTop / windowHeight * 100
+
+    barraProgreso.style.width = load + '%'
+}
+
+window.addEventListener('scroll', loadScroll)
+
 // HEADER
 
 const header = document.querySelector("#dc-header")
     window.addEventListener("scroll", () => {
     window.scrollY >= 50 ? header.classList.add("color-bg") : header.classList.remove("color-bg")
 })
+
+
