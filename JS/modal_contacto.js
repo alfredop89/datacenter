f4ad7e8447eebd0f687f0644a2c420eb5ecfc715
alfredop89 <offsound2020@gmail.com>
2022-09-12 13:14:41 -0400

    validando formulario

--- JS/modal_contacto.js
+++ JS/modal_contacto.js
@@ -10,22 +10,28 @@ const contactEmail = document.querySelector("#contactemail")
 const send = document.querySelector(".contactenviar")
 const textArea = document.querySelector("#textarea")
 
+let expresion = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+let validaciones = expresion.test(contactEmail.value)
+
 send.onclick = () =>{
-    if(contactNombre.value, contactEmail.value < 3){
+    if(contactNombre.value, contactEmail.value < 3 || contactNombre.value ==="" || textArea.value ===""){
         const contacttitulo = document.querySelector("#contacttitulo")
-        contacttitulo.innerText = "Datos inválidos"
+        contacttitulo.innerText = "No puedes dejar campos vacíos"
         return
     }
-    else if(contactNombre.value ===""){
-        const contacttitulo = document.querySelector("#contacttitulo")
-        contacttitulo.innerText = "Datos inválidos"
+    // else if(contactNombre.value ===""){
+    //     const contacttitulo = document.querySelector("#contacttitulo")
+    //     contacttitulo.innerText = "Datos inválidos"
+    //     return
+    // }
+    else if (!validaciones){
+        contacttitulo.innerText = "Correo electrónico inválido"
         return
     }
-    else if(textArea.value ===""){
-        const contacttitulo = document.querySelector("#contacttitulo")
-        contacttitulo.innerText = "No puedes enviar un mensaje vacío"
-        return
-    }
+    // else if(textArea.value ===""){
+    //     contacttitulo.innerText = "No puedes enviar un mensaje vacío"
+    //     return
+    // }
     else{
         contacttitulo.innerText = contactNombre.value + "," + " tu mensaje ha sido enviado. Gracias por contactarnos"
     }
